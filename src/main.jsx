36dedfbe0a19997b13f4efacaaba1951fdd9ac60
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-06-09 13:24:53 +0100

    project initial setup completed

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {createBrowserRouter,RouterProvider} from "react-router";
+
+
+const router = createBrowserRouter([
+
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+       <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
