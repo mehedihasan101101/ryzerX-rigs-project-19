fa18ecd7e4c6453ecfe7bab633889bbe99eeaed8
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-06-20 02:03:28 +0100

    max weidth fixed for medium and small devices of product archive section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import Categoryies from "../categories/Categoryies";
 
-import { Outlet} from "react-router";
+import { Outlet } from "react-router";
 import Herosection from "../heroSection/Herosection";
 
 
@@ -11,7 +11,7 @@ const Home = () => {
     <>
       <Herosection></Herosection>
       <h1 className=" text-black text-center text-3xl font-extrabold mt-10 mb-10">Discover Our Tech Collection</h1>
-      <div className="flex lg:max-w-[90%]  m-auto gap-5">
+      <div className="flex lg:max-w-[90%] max-w-[98%]  m-auto gap-5">
         <Categoryies></Categoryies>
         <Outlet> </Outlet>
       </div>
